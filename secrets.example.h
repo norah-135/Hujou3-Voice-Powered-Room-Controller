dc146aabbe764082acb73e43734bb7481d7b818c
norah-135 <norh13335@gmail.com>
2026-04-22 22:47:40 +0300

    Create secrets.example.h for configuration templates
    Added a template for network credentials and security tokens.

---
+++ secrets.example.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Secrets Template 
+
+// Network Credentials
+#define WIFI_SSID "YOUR_WIFI_NAME_HERE"
+#define WIFI_PASS "YOUR_WIFI_PASSWORD_HERE"
+
+// Hujou3 Security Token
+#define HUJOU3_AUTH_TOKEN "YOUR_MEOW_TOKEN_HERE"
+
+// Tuya IoT Cloud Keys
+#define TUYA_CLIENT_ID "YOUR_TUYA_ID"
+#define TUYA_CLIENT_SECRET "YOUR_TUYA_SECRET"
+#define TUYA_DEVICE_ID "YOUR_DEVICE_ID"
